68c6423a835fa4a143d4cc539503357ce21ffdc2
Lucie Langlois <lucie.langlois@hotmail.fr>
2021-09-21 13:38:59 +0200

    ajout projet mymoviz

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -32,7 +32,7 @@ function App() {
     async function loadData() {
       const response = await fetch('/new-movies')
       const jsonResponse = await response.json()
-      //console.log(jsonResponse)
+      console.log(jsonResponse)
       setMovieList(jsonResponse.movies)
 
       const responseWish = await fetch('wishlist-movie')
@@ -80,14 +80,14 @@ function App() {
     )
   })
 
-  // var moviesData = [
-  //   {name:"Star Wars : L'ascension de Skywalker", desc:"La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette ...", img:"/starwars.jpg", note:6.7, vote:5},
-  //   {name:"Maléfique : Le pouvoir du mal", desc: "Plusieurs années après avoir découvert pourquoi la plus célèbre méchante Disney avait un cœur ...", img:"/maleficent.jpg", note:8.2, vote:3},
-  //   {name:"Jumanji: The Next Level", desc: "L’équipe est de retour, mais le jeu a changé. Alors qu’ils retournent dans Jumanji pour secourir ...", img:"/jumanji.jpg", note:4, vote:5},
-  //   {name:"Once Upon a Time... in Hollywood", desc: "En 1969, Rick Dalton – star déclinante d'une série télévisée de western – et Cliff Booth ...", img:"/once_upon.jpg", note:6, vote:7},
-  //   {name:"La Reine des neiges 2", desc: "Elsa, Anna, Kristoff, Olaf et Sven voyagent bien au-delà des portes d’Arendelle à la recherche de réponses ...", img:"/frozen.jpg", note:4.6, vote:3},
-  //   {name:"Terminator: Dark Fate", desc: "De nos jours à Mexico. Dani Ramos, 21 ans, travaille sur une chaîne de montage dans une usine automobile...", img:"/terminator.jpg", note:6.1, vote:1},
-  // ]
+  /*  var moviesData = [
+    {name:"Star Wars : L'ascension de Skywalker", desc:"La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette ...", img:"/starwars.jpg", note:6.7, vote:5},
+    {name:"Maléfique : Le pouvoir du mal", desc: "Plusieurs années après avoir découvert pourquoi la plus célèbre méchante Disney avait un cœur ...", img:"/maleficent.jpg", note:8.2, vote:3},
+    {name:"Jumanji: The Next Level", desc: "L’équipe est de retour, mais le jeu a changé. Alors qu’ils retournent dans Jumanji pour secourir ...", img:"/jumanji.jpg", note:4, vote:5},
+    {name:"Once Upon a Time... in Hollywood", desc: "En 1969, Rick Dalton – star déclinante d'une série télévisée de western – et Cliff Booth ...", img:"/once_upon.jpg", note:6, vote:7},
+     {name:"La Reine des neiges 2", desc: "Elsa, Anna, Kristoff, Olaf et Sven voyagent bien au-delà des portes d’Arendelle à la recherche de réponses ...", img:"/frozen.jpg", note:4.6, vote:3},
+    {name:"Terminator: Dark Fate", desc: "De nos jours à Mexico. Dani Ramos, 21 ans, travaille sur une chaîne de montage dans une usine automobile...", img:"/terminator.jpg", note:6.1, vote:1},
+   ]  */
 
   var movieListItems = movieList.map((movie,i) => {
     var result = moviesWishList.find(element => element.name == movie.title)
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ var request = require('sync-request')
 
 var movieModel = require('../models/movies')
 
-var myApiKey = "8087f408100c3de021fda9e650563612"
+var myApiKey = "d949164e399b82c418f6805684262a40"
 
 router.get('/new-movies', function(req, res, next) {
+  console.log("NEW MOVIES ")
   var data = request('GET',`https://api.themoviedb.org/3/discover/movie?api_key=${myApiKey}&language=fr-FR&region=FR&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=2020-01-01`)
   var dataParse = JSON.parse(data.body)
-  
+  console.log("DATA FILMS",dataParse)
   res.json({result: true, movies:dataParse.results})
 });
 
